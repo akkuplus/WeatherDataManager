cad054f4162d0fc9679058ce9c22644072adada0
Marcus Strobel <marcus.strobel@gmx.de>
2019-10-01 22:23:24 +0200

    features: transformed script into a package
    - moved components of script in package "app" with modules -> two modules each with a object
    - moved calls of objects in main()-method into __init__-module

--- app/DataManager.py
+++ app/DataManager.py
@@ -5,89 +5,6 @@ zip code next to each weather data station. The WDM enriches weather data with i
 from weather data station, and appends the newest data to an existing SQL table.
 """
 
-
-class DataRequester(object):
-    """Requests and saves distant weather data into a local zip file."""
-
-    def __init__(self):
-        self.last_zip_file = ""
-        self.saved_zipfile_path = ""
-        self.url = ""
-
-    def run(self):
-        try:
-            print("\nDataRequester running..."
-                  "\n------------------------")
-            self.get_distant_filename()
-            self.save_zip_locally()
-            print("\nDataRequester closed."
-                  "\n---------------------\n")
-        except Exception as ex:
-            print(ex)
-
-    def get_distant_filename(self):
-        """Scrap HTML page to extract newest link."""
-        from bs4 import BeautifulSoup
-        import requests
-        from urllib.parse import urljoin
-
-        # extract current link in html file
-        self.url = "https://dbup2date.uni-bayreuth.de/blocklysql/"  # static link
-        html_file = "wetterdaten.html"  # static filename
-        html_path = urljoin(self.url, html_file)
-
-        zip_file_url = None
-        try:
-            resp = requests.get(html_path, verify=False)
-            soup = BeautifulSoup(resp.content, features="html.parser")
-            for link in soup.find_all('a', href=True):
-                if "CSV" in link['href']:
-                    zip_file_url = link['href']  # example: "downloads\wetterdaten\2019-07-07_wetterdaten_CSV.zip"
-                    break
-        except Exception as ex:
-            print("Connection to {} failed.".format(html_path))
-            print(ex)
-
-        # get distant zip file path
-        if zip_file_url:
-            self.last_zip_file = zip_file_url
-        else:
-            raise FileNotFoundError("Can't find a zipfile '*wetterdaten_CSV.zip' in HTML")
-
-    def save_zip_locally(self):
-        """Load last weather data saved in zip."""
-        # example: url = 'https://dbup2date.uni-bayreuth.de/blocklysql/downloads/wetterdaten/2019-06-21_wetterdaten.zip'
-        import requests
-        import urllib.parse
-        from pathlib import Path
-
-        # load last distant zip file from web
-        r = None
-        full_url_zip = None
-        try:
-            full_url_zip = urllib.parse.urljoin(self.url, self.last_zip_file)
-            r = requests.get(full_url_zip, allow_redirects=True, verify=False)
-
-            # get the filename of last zipfile
-            zip_file_name = self.last_zip_file.rsplit(sep="/", maxsplit=1)[-1]  # get the last element of url
-
-            # and create the "filepath" at "/data/*.zip"
-            self.saved_zipfile_path = Path.cwd().joinpath("data").joinpath(zip_file_name)
-
-        except Exception as ex:
-            print("Error loading {}".format(self.saved_zipfile_path))
-            print(ex)
-
-        # save the loaded file in "filepath"
-        try:
-            open(self.saved_zipfile_path, 'wb').write(r.content)
-            print("Saved {} from {}".format(self.saved_zipfile_path, full_url_zip))
-            dir(self.saved_zipfile_path)
-        except Exception as ex:
-            print("Error writing {}".format(self.saved_zipfile_path))
-            print(ex)
-
-
 class DataManager(object):
     """DataManager imports data of weather stations and weather measures,
     finds the nearest zip code for a given data station,
@@ -128,8 +45,9 @@ class DataManager(object):
             self.sql_from_weathermeasures()
             self.delete_old_weather_data()
             self.insert_new_weather_data()  # show changes in last date of weather date
-            print("\nDataManager closed."
-                  "\n-------------------\n")
+            print("\n----------------------"
+                  "\nDataManager closed."
+                  )
         except Exception as ex:
             print(ex)
 
@@ -418,21 +336,3 @@ class DataManager(object):
             return True
         else:
             return False
-
-
-if __name__ == "__main__":
-
-    # TODO: hide calls to methods in class __init__-methods
-    # TODO: separate classes into files
-    # TODO: log all outputs?!
-    # TODO: docker-version
-    # TODO: check relevant ports
-    # TODO: deploy realisation: docker, full setup or git pull?
-
-    # from WeatherDataManager import (DataRequester, DataManager)
-
-    dr = DataRequester()
-    dr.run()
-
-    dm = DataManager()
-    dm.run()
