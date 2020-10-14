1e8719647f7d32f2bd66853d5c10416dbac3e369
akkuplus <github@akkuplus.33mail.com>
2020-10-14 21:55:17 +0200

    - Refactored method get_distant_filename due to changed url - Added doc strings
    - Minor restructuring

--- app/DataRequester.py
+++ app/DataRequester.py
@@ -1,58 +1,60 @@
+from bs4 import BeautifulSoup
+import requests
 import app.Helper
 
 
+
 class DataRequester(object):
-    """Request and save remote weather data into a local zip file."""
+    """Request and save remote weather data locally in zip-file."""
 
     def __init__(self):
-
+        """Initialize Data Requester with default settings."""
         self.logger = app.Helper.MyLogger()
         self.logger.setup_handlers()
 
         self.last_zip_file = ""
         self.saved_zipfile_path = ""
-        self.url = "https://dbup2date.uni-bayreuth.de/blocklysql/"  # static link
+        self.url = "https://dbup2date.uni-bayreuth.de/downloads/wetterdaten/"  # static link
+        self.file_name = "wetterdaten_Wettermessung.csv"
+        # OLD self.url = "https://dbup2date.uni-bayreuth.de/blocklysql/"  # static link
 
-    def run(self):
-        try:
-            self.logger.info(f"DataRequester running...")
-            self.get_distant_filename()
-            self.save_zip_locally()
-            self.logger.info(f"DataRequester closed!")
-        except Exception as ex:
-            self.logger.exception(f"Error while running DataRequester")
+        return
 
     def get_distant_filename(self):
         """Scrap HTML page to extract newest link."""
-        from bs4 import BeautifulSoup
-        import requests
-        from urllib.parse import urljoin
 
-        # extract current link in html file
-        html_file = "wetterdaten.html"  # static filename
-        html_path = urljoin(self.url, html_file)
+        html_path = self.url
         zip_file_url = None
 
+        # extract current link in html file
         try:
             resp = requests.get(html_path, verify=False)
             soup = BeautifulSoup(resp.content, features="html.parser")
             self.logger.debug(f"Found website and parsed html-file")
-        except Exception:
-            self.logger.exception(f"Connection to {html_path} failed")
 
+            if resp.status_code == 404:
+                raise requests.HTTPError
+
+        except requests.HTTPError as e:
+            self.logger.error(f"Connection to {html_path} failed. URL does not exist ({e}).")
+            raise
+
+        # get distant zip file path
         try:
-            # get distant zip file path
             for link in soup.find_all('a', href=True):
-                if "CSV" in link['href']:
+                if "CSV.zip" in link['href']:
                     zip_file_url = link['href']  # example: "downloads\wetterdaten\2019-07-07_wetterdaten_CSV.zip"
                     break
-            if zip_file_url:
-                self.last_zip_file = zip_file_url
-                self.logger.debug(f"Found a zipfile in HTML-code")
-            else:
-                raise FileNotFoundError("Can't find a zipfile '*wetterdaten_CSV.zip' in HTML")
-        except Exception:
-            self.logger.exception(f"Error while extracting zipfile in HTML-code")
+
+            assert "_wetterdaten_CSV.zip" in zip_file_url, "Can't find a zipfile '*wetterdaten_CSV.zip' in HTML"
+            self.last_zip_file = zip_file_url
+            self.logger.debug(f"Found a zipfile in HTML-code")
+
+        except Exception as ex:
+            self.logger.exception(f"Error while extracting zipfile in HTML-code ({ex})")
+            raise
+
+        return
 
     def save_zip_locally(self):
         """Load last weather data that is saved in zip."""
@@ -86,3 +88,15 @@ class DataRequester(object):
             dir(self.saved_zipfile_path)
         except Exception:
             self.logger.exception(f"Error writing {self.saved_zipfile_path}")
+
+    def run(self):
+        """Process steps in order to collect new data."""
+        try:
+            self.logger.info(f"DataRequester running...")
+            self.get_distant_filename()
+            self.save_zip_locally()
+            self.logger.info(f"DataRequester closed!")
+        except Exception as ex:
+            self.logger.exception(f"Error while running DataRequester ({ex})")
+
+        return
