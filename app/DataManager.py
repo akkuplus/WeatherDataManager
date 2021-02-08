3c1e981af781973930c08f8bd9ecea07f7b9adc2
akkuplus <github@akkuplus.33mail.com>
2021-02-08 23:45:16 +0100

    - Bibliotheken oben angeordnet - Umbenennung zu "sql_fromweather_measures()" - Aufgeräumt Method Find_last_zipfile - Umbenennung zu "zipfile_exists()" - Namen der CSV-Dateien ins Config-Datei gesetzt - SQL-Abfrage zu PostgreSQL erfordert Spaltennamen als expliziten String -> Hochkomma eingefügt, da sonst Abbruch in SQL-Query in delete_old_weather_data() - verbesserte Struktur config.ini insert_new_weather_data()-Methode

--- app/DataManager.py
+++ app/DataManager.py
@@ -5,17 +5,21 @@ zip code next to each weather data station. The WDM enriches weather data with i
 from weather data station, and appends the newest data to an existing SQL table.
 """
 
-import pathlib
-import app.Helper
+import numpy as np
+import pandas as pd
+from pathlib import Path
 from sqlalchemy.sql import text
+import zipfile
+
+import app.Helper
 
 
 class DataManager(object):
     """
-	DataManager imports data of weather stations and weather measures,
+    DataManager imports data of weather stations and weather measures,
     finds the nearest zip code for a given data station,
     and imports new weather measure to SQL table.
-	"""
+    """
 
     def __init__(self):
 
@@ -32,8 +36,7 @@ class DataManager(object):
         self.saved_zipfile_path = None
         self.data_stations = None
         self.data_weather = None
-        # TODO: request name-parameters from configuration_file
-
+        return
 
     def run(self):
         try:
@@ -48,7 +51,7 @@ class DataManager(object):
             self.get_nearest_zipcode()
 
             self.database.reset_temporal_table()
-            self.sql_from_weathermeasures()
+            self.sql_from_weather_measures()
             self.delete_old_weather_data()
             self.insert_new_weather_data()  # show changes in last date of weather date
 
@@ -57,13 +60,10 @@ class DataManager(object):
         except Exception:
             self.logger.exception(f"Error while running DataManager")
 
-
-
     def find_last_zipfile(self):
         """
-		Finds the last zip file containing weather resources that is available in subdirectory 'resources'.
-		"""
-        from pathlib import Path
+        Find last zip file containing weather resources that is available in subdirectory 'resources'.
+        """
 
         # OLD: data_dir = self.config.get()
         data_dir = app.Helper.get_setting(["general", "data_dir"])
@@ -76,9 +76,9 @@ class DataManager(object):
             # The filename of all saved zipfiles includes a date part.
             # So sort these filenames and take last available zipfile.
             self.saved_zipfile_path = sorted(self.saved_zipfile_path.glob(
-                '????-??-??_wetterdaten_CSV.zip'),
-                reverse=True)[0]
-            if not self.last_zipfile_exists():
+                                            '????-??-??_wetterdaten_CSV.zip'),
+                                            reverse=True)[0]
+            if not self.zipfile_exists():
                 raise FileNotFoundError("Can't find a file with the pattern: '????-??-??_wetterdaten_CSV.zip'")
             else:
                 self.logger.info(f"Using last zipfile: '{self.saved_zipfile_path}'")
@@ -86,32 +86,28 @@ class DataManager(object):
         except FileNotFoundError:
             self.logger.exception()
 
-    def last_zipfile_exists(self):
+    def zipfile_exists(self):
         """
-		Ensure a given zipfile name is a valid file and contains
-         some information and has around 800 kB in subfolder '/resources/'.
-		 """
-        from pathlib import Path
+        Ensure a given zipfile name is a valid file,
+        contains some information,
+        and has around 800 kB.
+        """
 
-        try:
-            path_to_file = Path(self.saved_zipfile_path)
-            if path_to_file.is_file() and path_to_file.stat().st_size > 800000:
-                return True
-            else:
-                self.logger.warning(f"Can't determine a valid file: '{self.saved_zipfile_path}'")
-                return False
-        except Exception:
-            self.logger.exception()
+        path_to_file = Path(self.saved_zipfile_path)
+        if path_to_file.is_file() and path_to_file.stat().st_size > 800000:
+            return True
+        else:
+            self.logger.warning(f"Can't determine a valid file: '{self.saved_zipfile_path}'")
+            return False
 
     def import_weather_stations(self):
         """
-		Import weather stations and corresponding data
+        Import weather stations and corresponding data
         from locally saved zip file.
-		"""
-        import pandas as pd
-        import zipfile
+        """
 
-        file_name = 'wetterdaten_Wetterstation.csv'  # given filename in zip
+        #OLD file_name = 'wetterdaten_Wetterstation.csv'  # given filename in zip
+        file_name = app.Helper.get_setting(["general", "CSV_Name_Wetterstation"])
 
         # create DataFrame
         try:
@@ -131,12 +127,11 @@ class DataManager(object):
 
     def import_weather_measures(self):
         """
-		Import weather measures at a given station. The corresponding data is from saved zip file.
-		"""
-        import pandas as pd
-        import zipfile
+        Import weather measures at a given station. The corresponding data is from saved zip file.
+        """
 
-        file_name = 'wetterdaten_Wettermessung.csv'  # given filename in zip
+        # OLD file_name = 'wetterdaten_Wettermessung.csv'  # given filename in zip
+        file_name = app.Helper.get_setting(["general", "CSV_Name_Wettermessung"])
 
         # create DataFrame
         try:
@@ -157,11 +152,9 @@ class DataManager(object):
 
     def import_locational_data(self):
         """
-		Import locational data from a local csv file that contains a mapping of zip codes
-          and their corresponding coordinates.
-		 """
-        import pandas as pd
-        import numpy as np
+        Import locational data from a local csv file that contains a mapping of zip codes
+        and their corresponding coordinates.
+        """
 
         #data_dir = self.config.get("general", "data_dir")
         data_dir = app.Helper.get_setting(["general", "data_dir"])
@@ -189,7 +182,7 @@ class DataManager(object):
                     'Ort': np.str,
                     'Latitude': np.float,
                     'Longitude': np.float},
-                encoding='cp1250'  # converts typical german encoding
+                encoding='cp1250'  # converts german encoding
                 # converters = {"Plz": cast_to_str}
             )
             self.logger.info("Imported into DataFrame")
@@ -198,7 +191,6 @@ class DataManager(object):
 
     def enrich_data_stations(self):
         """Appends four columns to existing DataFrame 'data_stations'."""
-        import numpy as np
 
         try:
             appended_columns = self.mapping_zipcode_coordinates.columns + "_matched"
@@ -213,7 +205,6 @@ class DataManager(object):
 
     def find_nearest_zipcode(self, row):
         """ Find the nearest zip code for a given data station by minimal distance of coordinates."""
-        import numpy as np
 
         # a row describes a data_station
         station_para_lat = row["Geo_Breite"]
@@ -246,29 +237,33 @@ class DataManager(object):
         except Exception:
             self.logger.exception("Can't map a data_station to a zipcode")
 
-    def sql_from_weatherstations(self):
+    def sql_from_weather_measures(self):
         try:
-            self.data_stations.to_sql('Wettermessung', con=self.database.connection, if_exists='append', index=False)
-            self.logger.info(f"Transferred DataFrame 'stations' to Database: '{self.database.connection}'")
+            table_name = "temporal_measures"
+            self.data_weather.to_sql(table_name, con=self.database.connection, if_exists='replace', index=False)
+            self.logger.info(f"Transferred DataFrame containing weather data to temporary table '{table_name}'")
         except Exception:
             self.logger.exception("Error transferring DataFrame to Database")
 
-    def sql_from_weathermeasures(self):
+    def sql_from_weather_stations(self):
+        """
+        Helper-method to import weather station data into sql table.
+        """
         try:
-            table_name = "temp"
-            self.data_weather.to_sql(table_name, con=self.database.connection, if_exists='replace', index=False)
-            self.logger.info(f"Transferred DataFrame containing weather data to temporary table '{table_name}'")
+            table_name = 'measures'
+            self.data_stations.to_sql(table_name, con=self.database.connection, if_exists='replace', index=False)
+            self.logger.info(f"Transferred DataFrame 'stations' to Database: '{self.database.connection}'")
         except Exception:
             self.logger.exception("Error transferring DataFrame to Database")
 
     def delete_old_weather_data(self):
         """Deletes all existing data rows in temporary table that have an
         identical combination of Station_ID and Datum within the complete database table"""
-
+        temporal_table_name = "temporal_measures"
         try:
             self.database.connection.execute(
-                text("DELETE FROM temp WHERE (Stations_ID, Datum) "
-                     "IN (SELECT DISTINCT Stations_ID, Datum FROM Wettermessung);"
+                text(f"DELETE FROM {temporal_table_name} WHERE ('{temporal_table_name}.Stations_ID', '{temporal_table_name}.Datum') "
+                     "IN (SELECT DISTINCT 'Stations_ID', 'Datum' FROM measures);"
                      ))
             self.logger.debug("Cleared duplicate data from temporary table")
         except Exception:
@@ -276,14 +271,23 @@ class DataManager(object):
 
     def insert_new_weather_data(self):
 
-        # TODO: get information from config_file
+        temporal_table_name = "temporal_measures"
         try:
-            latest_dates = {"measure_before": self.database.connection.execute(text("SELECT Max(Datum) from Wettermessung;")).fetchall(),
-                            "temporal": self.database.connection.execute(text("SELECT Max(Datum) from temp;")).fetchall()}
+            # GET last Date before Insert
+            latest_dates = {
+                "measure_before": self.database.connection.execute(text("SELECT Max(Datum) from measures;")).fetchall(),
+                "temporal": self.database.connection.execute(text(f"SELECT Max(Datum) from {temporal_table_name};")).fetchall()
+            }
 
+            # Insert transaction
             self.database.connection.execute(
-                text('INSERT INTO Wettermessung SELECT * FROM temp;'))
-            latest_dates["measure_after"] = self.database.connection.execute(text("SELECT Max(Datum) from Wettermessung;")).fetchall()
+                text(f"INSERT INTO Wettermessung SELECT * FROM {temporal_table_name};")
+            )
+
+            # GET last Date after Insert
+            latest_dates["measure_after"] = self.database.connection.execute(text("SELECT Max(Datum) from measures;")).fetchall()
+
+            # SHOW newer dates
             self.logger.info(f"Updated weather data: to new LATEST date {latest_dates['measure_after']} " 
                              f"from old lasted date {latest_dates['measure_before']}")
             # print("Updated weather data measure. \n"
@@ -292,3 +296,4 @@ class DataManager(object):
             #        after=latest_dates["measure_after"]))
         except Exception:
             self.logger.exception("Error updating/integrating new weather_data into permanent database table {}")
+
