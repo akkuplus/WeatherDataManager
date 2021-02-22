6f5fb39a305100951b0e8ba1200a995618a7eb55
akkuplus <github@akkuplus.33mail.com>
2021-02-22 02:45:50 +0100

    DockerFile für Weather-Service
    Docker-Compose File verbindet Weather-Service und Postgres-Service.
    
    Im Postgres-Service wird Rolle "postgres" und Datenbank "wetterdaten" per Skript init.sql angelegt.

---
+++ config/init.sql
@@ -0,0 +1,9 @@
+create database "wetterdaten";
+GRANT ALL PRIVILEGES ON DATABASE "wetterdaten" to postgres;
+
+-- create database "testdb";
+-- GRANT ALL PRIVILEGES ON DATABASE "testdb" to postgres;
+
+
+-- https://graspingtech.com/docker-compose-postgresql/
+-- https://onexlab-io.medium.com/docker-compose-postgres-initdb-ba0021deef76
